82ad4ba3ae67fe2f219ba2de36c290b1af5767e5
emamyari <emamyari@yahoo.com>
2024-02-22 11:35:58 +0330

    router browser was developed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 import { Productlist } from './Components/Productlist/Productlist';
+import { Shop } from './Components/Shop/Shop';
 import { Navbar } from './Components/Navbar/Navbar';
+import {
+  BrowserRouter ,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
   return (
     <div>
-      <Navbar/>
-      <Productlist />
+      <BrowserRouter>
+        {/* <Navbar/> */}
+        <Routes>
+        <Route path="/" element={<Shop/>} />
+        <Route path="/men" element={<Productlist/>} />
+        <Route path="/women" element={<Productlist/>} />
+        <Route path="/kid" element={<Productlist/>} />
+
+
+        </Routes>
+
+      </BrowserRouter>
     </div>
     );
 }
