d5fba67e554c8fe0a93bbf9af1b1311e36b0cbf6
emamyari <emamyari@yahoo.com>
2024-02-29 11:23:25 +0330

    add one component.

---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    <div>ShopShopShopShopShopShopShopShopShopShopShop</div>
+    </div>
+  )
+}
