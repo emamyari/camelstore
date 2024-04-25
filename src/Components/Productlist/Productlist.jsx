49f6894cd75c3cead9bd306a1e81419d7dab414a
emamyari <emamyari@yahoo.com>
2024-04-25 11:25:43 +0330

    add increment logic to product list.

--- src/Components/Productlist/Productlist.jsx
+++ src/Components/Productlist/Productlist.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-
+import './Productlist.css'
 export const Productlist = (props) => {
     const [state, setSate] = useState([])
+    const [Plist, setPlist] = useState([])
     useEffect(
         () => {
-            reload()   
-         },[]
+            reload()
+        }, []
 
     )
 
+    function inc(title){
+
+        let product=Plist.filter(p=>p.name==title)[0]
+        
+        if(product==null){
+            product={name:title,count:1};
+        }else{
+            product.count++;
+        }
+        let lst=[];
+        lst=Plist.filter(p=>p.name!=title)
+        lst.push(product);
+        setPlist(lst)
+    }
+    function dec(title){
+        console.log(Plist)
+    }
     function reload() {
- 
+
         var requestOptions = {
             method: 'GET',
             redirect: 'follow'
-          };
-          console.log(props.category)
-          fetch("https://mobapi.banimode.com/api/v4/products?platform=desktop&page_size=24&page=2&filter[product_categories.id][eq]="+props.category, requestOptions)
+        };
+        console.log(props.category)
+        fetch("https://mobapi.banimode.com/api/v4/products?platform=desktop&page_size=24&page=2&filter[product_categories.id][eq]=" + props.category, requestOptions)
             .then(response => response.json())
             .then(result => setSate(result.data.data))
             .catch(error => console.log('error', error));
- 
+
     }
 
     return (
@@ -30,6 +48,7 @@ export const Productlist = (props) => {
                 <div className="row">
                     <div className="col-md-12">
                         {state.map(c => <div className="col-sm-6 col-md-3">
+
                             <div className="thumbnail" >
                                 <img src={c.images.large_default[0]} className="img-responsive"></img>
                                 <div className="caption">
@@ -42,12 +61,15 @@ export const Productlist = (props) => {
                                                 <label>{c.product_price}</label></h6>
                                         </div>
                                         <div className="col-md-6 col-xs-6 price">
-                                        <p>{c.color_name}</p>
+                                            <p>{c.color_name}</p>
 
                                         </div>
 
                                     </div>
-
+                                    <div className='divbtn'>
+                                        <button onClick={()=>inc(c.product_name)} className='btn btn-success mx-auto btn-green' >+</button>
+                                        <button onClick={()=>dec(c.product_name)}  className='btn btn-danger mx-auto btn-red'>-</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -58,7 +80,7 @@ export const Productlist = (props) => {
                 </div>
             </div>
 
-            
+
 
         </div>
     )
